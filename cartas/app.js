8199ad3b1df10b1b0c386f0fe3b6cc8b93025867
ANNA LUIZA FERREIRA COSTA DOS SANTOS <anna.lfcsantos@senacsp.edu.br>
2025-12-03 14:59:39 -0300

    Ajustes nas cartas

--- cartas/app.js
+++ cartas/app.js
@@ -22,17 +22,45 @@ function sortear(){
     // console.log(face)
     // console.log(nipe)
     
+    // Determinar a cor com base no nipe sorteado
+
+    let cor
+    if (nipe === "♥" || nipe === "♦") {
+        cor = "#ff0000"
+        
+    } else {
+        cor="#000"
+    }
 
     // Renderização do canto superior esquerdo da carta 
     //.innerHTML => Incere uma tag no documento HTML
     // Esta linha insere o conteúdo das variávei no documento
     document.getElementById('supEsq').innerHTML = `<div>${face}</div> <div>${nipe}</div>`
+    // Mudar a cor
+    document.getElementById('supEsq').style.color = cor
+    // O JS pode também manipular o css usando .style
 
 
     // Renderização do centro da carta 
-    document.getElementById('centroCarta').innerHTML = `<div>${nipe}</div>`
+    let cc = document.getElementById('centroCarta')
+    if (face === 'J'){
+        cc.innerHTML = '<img src="./img/valete.png">'
+    } else if (face === 'Q'){
+        cc.innerHTML = '<img src="./img/dama.png">'
+    } else if (face === 'K'){
+        cc.innerHTML = '<img src="./img/rei.png">'
+    }
+    else{
+        cc.innerHTML = nipe
+        cc.style.color = cor
+    }
+
+    //document.getElementById('centroCarta').innerHTML = `<div>${nipe}</div>`
+    //document.getElementById('centroCarta').style.color = cor
 
 
     // Renderização do canto inferior Direito da carta 
      document.getElementById('infDir').innerHTML = `<div>${face}</div> <div>${nipe}</div>`
+    //  Mudar a cor
+     document.getElementById('infDir').style.color = cor
 }
